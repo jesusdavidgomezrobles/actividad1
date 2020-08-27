5504d0431c869a9d06c4ccb7c39debe8bfd912fa
jesus gomez <jesusdavidgomez@uniguajira.edu.co>
2020-08-26 21:27:10 -0500

    numeros primos

---
+++ programa2.py
@@ -0,0 +1,18 @@
+
+n = int(input("ingrese un numero"))
+
+def evaluar_primo(n):
+    contador=0
+    resultado=True
+    
+    for i in range(1, n+1):
+        if (n%i==0):
+            contador+=1
+        if (contador>2):
+            resultado=False
+            break
+    return resultado
+if (evaluar_primo(n)==True):
+    print("El numero ingresado es primo")
+else:
+    print("El numero ingresado no es primo")
